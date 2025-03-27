908a9769267c5a6af4271fefaa1fe975c0513250
ttdo19 <thuytrang19072001@gmail.com>
2025-03-27 00:47:06 -0500

    Last fix on the font

--- src/app/questions/question.tsx
+++ src/app/questions/question.tsx
@@ -62,7 +62,7 @@ export default function Question({animeImage, answerImage, question, possibleAns
                             return <button 
                             key={ans.id}
                             onClick={()=> handleClick(ans.id)} 
-                            className="rounded-[8px] border-solid w-[250px] min-2xl:w-[400px] border-[2px] m-[0.5rem] border-black px-[1.2em] py-[0.6em] font-medium bg-[#C1C3C1] cursor-pointer text-black hover:bg-[#1A97DB] hover:text-white">
+                            className="rounded-[8px] border-solid w-[250px] min-md:w-[300px] min-2xl:w-[400px] border-[2px] m-[0.5rem] border-black px-[1.2em] py-[0.6em] font-medium bg-[#C1C3C1] cursor-pointer text-black hover:bg-[#1A97DB] hover:text-white">
                                 {ans.text}
                             </button>
                         })}
@@ -72,7 +72,7 @@ export default function Question({animeImage, answerImage, question, possibleAns
         );
     } else {
         return (
-            <motion.div className="flex flex-col items-center text-lg min-sm:text-xl my-[0.25em]"
+            <motion.div className="flex flex-col items-center text-lg min-sm:text-2xl min-2xl:text-4xl my-[0.25em]"
                 variants={variants}
                 initial='initial'
                 animate='visible'
@@ -81,7 +81,7 @@ export default function Question({animeImage, answerImage, question, possibleAns
                     {choice && "You chose correctly🎉🎉🎉"}
                     {!choice && "Oops!🫣🙀"}
                 </h1>
-                <h1>{!choice && "Maybe you will get the next one!"}</h1>
+                <h1>{currentIndex!== 4 && !choice && "Maybe you will get the next one!"}</h1>
                 <div className="flex flex-row flex-wrap gap-[20px] justify-center my-[0.25em] ">
                     <div className="w-[50vw] min-sm:w-[30vw]">
                         <Image
@@ -103,7 +103,7 @@ export default function Question({animeImage, answerImage, question, possibleAns
                 <h1>
                 {possibleAnswers[correctAnswer].text}
                 </h1>
-                <button onClick={nextQuestion} className="min-sm:text-xl w-[100px] min-2xl:w-[400px] text-lg rounded-[8px] border-solid border-[2px] p-[0.3em] min-sm:m-[0.5rem] border-black min-sm:px-[1.2em] min-sm:py-[0.6em] font-medium bg-[#C1C3C1] cursor-pointer text-black hover:bg-[#1A97DB] hover:text-white">
+                <button onClick={nextQuestion} className="min-sm:text-xl w-[100px] min-md:w-[200px] text-lg rounded-[8px] border-solid border-[2px] p-[0.3em] min-sm:m-[0.5rem] border-black min-sm:px-[1.2em] min-sm:py-[0.6em] font-medium bg-[#C1C3C1] cursor-pointer text-black hover:bg-[#1A97DB] hover:text-white">
                     Continue
                 </button>
             </motion.div>
